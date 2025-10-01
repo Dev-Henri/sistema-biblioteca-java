fb02808e012012f11d75cd22a711ef830d67067c
Henrique Silva <henriquesilva12332@gmail.com>
2025-10-01 20:44:43 -0300

    Criação da classe biblioteca

--- src/entities/Library.java
+++ src/entities/Library.java
@@ -1,5 +1,40 @@
 package entities;
 
-public class Library {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Library {
+	
+	private List<Books> livros;
+	private List<User> usuarios;
+	
+	public Library() {
+		this.livros = new ArrayList<>();
+		this.usuarios = new ArrayList<>();
+	}
+	
+	public List<Books> getBooks(){
+		return livros;
+	}
+	
+	public List<User> getUser(){
+		return usuarios;
+	}
+	
+	public void adicionarLivro(Books livro) {
+		livros.add(livro);
+	}
+	
+	public void cadastrarUsuario(User usuario) {
+		usuarios.add(usuario);
+	}
+	
+	public void listarLivrosDisponivel() {
+		System.out.println("Livros disponíveis:");
+		for(Books l : livros) {
+			if(l.isDisponivel()) {
+				System.out.printf("- ", l);
+			}
+		}
+	}
 }
